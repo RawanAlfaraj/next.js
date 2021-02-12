02ccac7b337e8aaa70788a6cbe200b95ffd0754b
Rawan <sk8r0rawan@gmail.com>
2021-02-12 18:06:20 +0300

    postcssconfig

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 const purgecss = [
-    '@fullhuman/postcss-purgecss',
+   
     {
-      // Specify the paths to all of the template files
-      content: [
-        './pages/**/*.{js,jsx,ts,tsx}',
-        './components/**/*.{js,jsx,ts,tsx}',
-      ],
       // This is the function used to extract class names from the templates
       defaultExtractor: (content) => {
         // Capture as liberally as possible, including things like `h-(screen-1.5)`
